f2cfdd720e63fd0320c8dc7f77fe44bfa7cc20a8
Rajeshwari05 <sirigirirajeshwari1234@gmail.com>
2024-11-08 12:02:26 +0530

    Calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,17 +2,25 @@ let string = " ";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
+
         if(e.target.innerHTML == "="){
             string = eval(string);
             document.querySelector('input').value=string;
             console.log(string);
+        }
+
+        else if(e.target.innerHTML == "backspace"){
+            string = string.substr(0, string.length-1);
+            document.querySelector('input').value=string;
 
         }
+
         else if(e.target.innerHTML == "AC"){
             string= " ";
             document.querySelector("input").value=string;
                 console.log(string)
         }
+
         else{
         console.log(e.target);
         string = string + e.target.innerHTML;
