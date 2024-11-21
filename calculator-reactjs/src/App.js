ac40f784209171494da25e69014e6a43f6e92cca
Rajeshwari05 <sirigirirajeshwari1234@gmail.com>
2024-11-21 15:41:16 +0530

    HPS-React-Calculator

---
+++ calculator-reactjs/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Calculator from './Calculator';
+function App() {
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
