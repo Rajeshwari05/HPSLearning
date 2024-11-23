61997303751e47ddab6a9e1e79a8d75d539d4fd6
Rajeshwari05 <sirigirirajeshwari1234@gmail.com>
2024-11-23 14:47:40 +0530

    HPS-reactCalculator

---
+++ react-calculator/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Calculator</h1>
+      </header>
+      <div className='Container'>
+        <div className="calculator">
+          <Calculator />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
