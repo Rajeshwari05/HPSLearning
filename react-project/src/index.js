d30300a457f41620bd88437debbcf2e2b863fbc4
Rajeshwari05 <sirigirirajeshwari1234@gmail.com>
2024-12-30 20:33:30 +0530

    Updated React-Project

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -23,9 +23,10 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App />
+        </Provider>,
   document.getElementById("root")
 );
